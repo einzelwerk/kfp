d363e7d171b841fde95e685257991f102eab7759
grade33 <baledg3@gmail.com>
2023-04-04 05:15:48 +0400

    Deploy Hero section

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+set -e
+
+npm run build
+
+git add .
+git commit -m "Deploy $1"
+git push origin main
+git push origin2 main
+git subtree push --prefix dist origin gh-pages
+git subtree push --prefix dist origin2 gh-pages
