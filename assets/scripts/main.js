1626b368dcdbff50da0d60a78ed8ed071270bf92
grade33 <baledg3@gmail.com>
2023-04-05 13:36:29 +0400

    Deploy Offer, Program sections

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,119 @@
 /******/ 	"use strict";
 var __webpack_exports__ = {};
 
+;// CONCATENATED MODULE: ./node_modules/handy-collapse/dist/handy-collapse.mjs
+var c = Object.defineProperty;
+var g = (l, t, s) => t in l ? c(l, t, { enumerable: !0, configurable: !0, writable: !0, value: s }) : l[t] = s;
+var r = (l, t, s) => (g(l, typeof t != "symbol" ? t + "" : t, s), s);
+class p {
+  constructor(t = {}) {
+    r(this, "toggleContentEls");
+    r(this, "toggleButtonEls");
+    r(this, "itemsState", {});
+    r(this, "options");
+    const s = typeof t == "object" && "nameSpace" in t ? t.nameSpace : "hc", i = {
+      nameSpace: "hc",
+      toggleButtonAttr: `data-${s}-control`,
+      toggleContentAttr: `data-${s}-content`,
+      activeClass: "is-active",
+      isAnimation: !0,
+      closeOthers: !0,
+      animationSpeed: 400,
+      cssEasing: "ease-in-out",
+      onSlideStart: () => {
+      },
+      onSlideEnd: () => {
+      }
+    };
+    this.options = {
+      ...i,
+      ...t
+    }, this.toggleContentEls = [].slice.call(document.querySelectorAll(`[${this.options.toggleContentAttr}]`)), this.toggleButtonEls = [].slice.call(document.querySelectorAll(`[${this.options.toggleButtonAttr}]`)), this.toggleContentEls.length !== 0 && this.initContentsState(this.toggleContentEls), this.toggleButtonEls.length !== 0 && this.handleButtonsEvent(this.toggleButtonEls);
+  }
+  initContentsState(t) {
+    this.itemsState = {}, t.forEach((s) => {
+      s.style.overflow = "hidden", s.style.maxHeight = "none";
+      const i = s.classList.contains(this.options.activeClass), e = s.getAttribute(this.options.toggleContentAttr);
+      !e || (this.setItemState(e, i), i ? this.open(e, !1, !1) : this.close(e, !1, !1));
+    });
+  }
+  handleButtonsEvent(t) {
+    t.forEach((s) => {
+      const i = s.getAttribute(this.options.toggleButtonAttr);
+      i && s.addEventListener(
+        "click",
+        (e) => {
+          e.preventDefault(), this.toggleSlide(i, !0);
+        },
+        !1
+      );
+    });
+  }
+  setItemState(t, s) {
+    this.itemsState[t] = {
+      isOpen: s,
+      isAnimating: !1
+    };
+  }
+  toggleSlide(t, s = !0) {
+    var i, e;
+    (i = this.itemsState[t]) != null && i.isAnimating || (((e = this.itemsState[t]) == null ? void 0 : e.isOpen) === !1 ? this.open(t, s, this.options.isAnimation) : this.close(t, s, this.options.isAnimation));
+  }
+  open(t, s = !0, i = !0) {
+    if (!t)
+      return;
+    Object.prototype.hasOwnProperty.call(this.itemsState, t) || this.setItemState(t, !1);
+    const e = document.querySelector(`[${this.options.toggleContentAttr}='${t}']`);
+    if (!e)
+      return;
+    this.itemsState[t].isAnimating = !0, this.options.closeOthers && [].slice.call(this.toggleContentEls).forEach((n) => {
+      const h = n.getAttribute(this.options.toggleContentAttr);
+      h && h !== t && this.close(h, !1, i);
+    }), s !== !1 && this.options.onSlideStart(!0, t);
+    const a = this.getTargetHeight(e);
+    e.style.visibility = "visible", e.classList.add(this.options.activeClass);
+    const o = document.querySelectorAll(`[${this.options.toggleButtonAttr}='${t}']`);
+    o.length > 0 && [].slice.call(o).forEach((n) => {
+      n.classList.add(this.options.activeClass), n.hasAttribute("aria-expanded") && n.setAttribute("aria-expanded", "true");
+    }), i ? (e.style.overflow = "hidden", e.style.transition = `${this.options.animationSpeed}ms ${this.options.cssEasing}`, e.style.maxHeight = (a || "1000") + "px", setTimeout(() => {
+      s !== !1 && this.options.onSlideEnd(!0, t), e.style.maxHeight = "none", e.style.transition = "", e.style.overflow = "", this.itemsState[t].isAnimating = !1;
+    }, this.options.animationSpeed)) : (e.style.maxHeight = "none", e.style.overflow = "", this.itemsState[t].isAnimating = !1), this.itemsState[t].isOpen = !0, e.hasAttribute("aria-hidden") && e.setAttribute("aria-hidden", "false");
+  }
+  close(t, s = !0, i = !0) {
+    if (!t)
+      return;
+    Object.prototype.hasOwnProperty.call(this.itemsState, t) || this.setItemState(t, !1), this.itemsState[t].isAnimating = !0, s !== !1 && this.options.onSlideStart(!1, t);
+    const e = document.querySelector(`[${this.options.toggleContentAttr}='${t}']`);
+    e.style.overflow = "hidden", e.classList.remove(this.options.activeClass), e.style.maxHeight = e.clientHeight + "px", setTimeout(() => {
+      e.style.maxHeight = "0px";
+    }, 5);
+    const a = document.querySelectorAll(`[${this.options.toggleButtonAttr}='${t}']`);
+    a.length > 0 && [].slice.call(a).forEach((o) => {
+      o.classList.remove(this.options.activeClass), o.hasAttribute("aria-expanded") && o.setAttribute("aria-expanded", "false");
+    }), i ? (e.style.transition = `${this.options.animationSpeed}ms ${this.options.cssEasing}`, setTimeout(() => {
+      s !== !1 && this.options.onSlideEnd(!1, t), e.style.transition = "", this.itemsState[t].isAnimating = !1, e.style.visibility = "hidden";
+    }, this.options.animationSpeed)) : (this.options.onSlideEnd(!1, t), this.itemsState[t].isAnimating = !1, e.style.visibility = "hidden"), Object.prototype.hasOwnProperty.call(this.itemsState, t) && (this.itemsState[t].isOpen = !1), e.hasAttribute("aria-hidden") && e.setAttribute("aria-hidden", "true");
+  }
+  getTargetHeight(t) {
+    if (!t)
+      return;
+    const s = t.cloneNode(!0), i = t.parentNode;
+    if (!i)
+      return;
+    const e = [].slice.call(s.querySelectorAll("input[name]"));
+    if (e.length !== 0) {
+      const o = "-" + new Date().getTime();
+      e.forEach((n) => {
+        n.name += o;
+      });
+    }
+    s.style.maxHeight = "none", s.style.opacity = "0", i.appendChild(s);
+    const a = s.clientHeight;
+    return i.removeChild(s), a;
+  }
+}
+
+
 ;// CONCATENATED MODULE: ./node_modules/ssr-window/ssr-window.esm.js
 /**
  * SSR Window 4.0.2
@@ -4764,9 +4877,9 @@ function create_element_if_not_defined_createElementIfNotDefined(swiper, origina
   if (swiper.params.createElements) {
     Object.keys(checkProps).forEach(key => {
       if (!params[key] && params.auto === true) {
-        let element = elementChildren(swiper.el, `.${checkProps[key]}`)[0];
+        let element = utils_elementChildren(swiper.el, `.${checkProps[key]}`)[0];
         if (!element) {
-          element = createElement('div', checkProps[key]);
+          element = utils_createElement('div', checkProps[key]);
           element.className = checkProps[key];
           swiper.el.append(element);
         }
@@ -4861,7 +4974,7 @@ function Navigation({
   }
   function init() {
     const params = swiper.params.navigation;
-    swiper.params.navigation = createElementIfNotDefined(swiper, swiper.originalParams.navigation, swiper.params.navigation, {
+    swiper.params.navigation = create_element_if_not_defined_createElementIfNotDefined(swiper, swiper.originalParams.navigation, swiper.params.navigation, {
       nextEl: 'swiper-button-next',
       prevEl: 'swiper-button-prev'
     });
@@ -7369,7 +7482,7 @@ function Autoplay({
   };
   const onVisibilityChange = () => {
     if (swiper.destroyed || !swiper.autoplay.running) return;
-    const document = getDocument();
+    const document = ssr_window_esm_getDocument();
     if (document.visibilityState === 'hidden') {
       pausedByInteraction = true;
       pause(true);
@@ -7400,11 +7513,11 @@ function Autoplay({
     swiper.el.removeEventListener('pointerleave', onPointerLeave);
   };
   const attachDocumentEvents = () => {
-    const document = getDocument();
+    const document = ssr_window_esm_getDocument();
     document.addEventListener('visibilitychange', onVisibilityChange);
   };
   const detachDocumentEvents = () => {
-    const document = getDocument();
+    const document = ssr_window_esm_getDocument();
     document.removeEventListener('visibilitychange', onVisibilityChange);
   };
   on('init', () => {
@@ -8705,13 +8818,20 @@ var classNames = {
     },
     memorable: {
       sliderBlock: 'memorable__slider-block',
-      slider: 'memorable__slider'
-    }
+      slider: 'memorable__slider',
+      btnPrev: 'memorable__slider-btn_prev',
+      btnNext: 'memorable__slider-btn_next'
+    },
+    program: 'program__slider'
   },
   sliderProgress: {
     fill: 'swiper-progress-fill',
     total: 'swiper-progress-text_total'
-  }
+  },
+  imgTabs: [{
+    btn: 'program__acc-head',
+    img: 'program__img'
+  }]
 };
 ;// CONCATENATED MODULE: ./src/scripts/modules/sliders.js
 
@@ -8747,6 +8867,11 @@ function initMemorableSlider() {
   var sliderBlockClass = classNames.slider.memorable.sliderBlock;
   var sliderClass = classNames.slider.memorable.slider;
   var swiper = new core(".".concat(sliderClass), {
+    modules: [Navigation],
+    navigation: {
+      prevEl: ".".concat(classNames.slider.memorable.btnPrev),
+      nextEl: ".".concat(classNames.slider.memorable.btnNext)
+    },
     spaceBetween: 16,
     speed: 700,
     keyboard: true,
@@ -8756,10 +8881,49 @@ function initMemorableSlider() {
   fillSliderProgress(sliderBlock, swiper);
   return swiper;
 }
+function initProgramSlider() {
+  var sliderClass = classNames.slider.program;
+  return new core(".".concat(sliderClass), {
+    modules: [Autoplay],
+    autoplay: {
+      delay: 2000
+    },
+    spaceBetween: 16,
+    speed: 700,
+    rewind: true
+  });
+}
+;// CONCATENATED MODULE: ./src/scripts/modules/initImgTabs.js
+
+function initAccImgTabs() {
+  classNames.imgTabs.forEach(function (imgTabItem) {
+    var classBtn = imgTabItem.btn;
+    var classImg = imgTabItem.img;
+    var img = document.querySelector(".".concat(classImg));
+    document.querySelectorAll(".".concat(classBtn)).forEach(function (btn, idx) {
+      btn.addEventListener('click', function () {
+        img.src = btn.dataset.imgSrc;
+      });
+      if (idx === 0) btn.click();
+    });
+  });
+}
 ;// CONCATENATED MODULE: ./src/scripts/app.js
 
 
+
+
+
+// Accordion
+// eslint-disable-next-line
+new p();
+
+// Img Tabs
+initAccImgTabs();
+
+// Sliders
 initBenefitsSlider();
 initMemorableSlider();
+initProgramSlider();
 /******/ })()
 ;
